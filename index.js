dd050c2399323778ec8832327b752c6a49d23895
Naman Jha <jhanaman786@gmail.com>
2022-11-25 21:47:23 +0530

    fixed title and margins

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ document.querySelector(".roll").addEventListener("click", function () {
     document.querySelector(".img2").setAttribute("src", randomImageSource2);
 
     if (randomNumber1 > randomNumber2) {
-        document.querySelector("h1").textContent = "Player 1 Wins🚩";
+        document.querySelector("#message").innerHTML = "Player 1 Wins🚩";
     } else if (randomNumber1 < randomNumber2) {
-        document.querySelector("h1").textContent = "Player 2 Wins🚩";
+        document.querySelector("#message").innerHTML = "Player 2 Wins🚩";
     } else {
-        document.querySelector("h1").textContent = "Draw!";
+        document.querySelector("#message").innerHTML = "Draw!";
     }
 })
